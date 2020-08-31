1dc8857e9239abe0a6ad6bc9e1813e8851c8cbf9
seckboy <e_ford.geo@yahoo.com>
2020-08-31 01:00:04 -0500

    add with to tracer

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -85,12 +85,12 @@ def index():
     if request.method == 'GET':
 
         # Get current values
-        vote1 = r.get(button1).decode('utf-8')
+        with tracer.span(name="cat"):
+            vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tracer.span(name="cat")
-        vote2 = r.get(button2).decode('utf-8')
+        tracer.span(name="dog"):
+            vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tracer.span(name="dog")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
